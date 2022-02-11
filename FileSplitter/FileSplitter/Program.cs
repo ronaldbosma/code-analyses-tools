e31a8d64a97ad629731d553a950c4c6cb87585e8
Ronald Bosma <ronald.bosma@infosupport.com>
2022-02-11 14:14:45 +0100

    Add file splitter

---
+++ FileSplitter/FileSplitter/Program.cs
@@ -0,0 +1,31 @@
+﻿const string file = @"C:\repos\foo\big-file.sql";
+const string textToSplitOn = "GO";
+
+const string outputFileFormat = @"C:\repos\foo\split-file-{0}.sql";
+int counter = 1;
+
+
+using var reader = new StreamReader(file);
+
+
+var outputFile = string.Format(outputFileFormat, counter++);
+var writer = new StreamWriter(outputFile);
+
+while (!reader.EndOfStream)
+{
+    var line = reader.ReadLine();
+    if (line != null)
+    {
+        if (line.Trim().Equals(textToSplitOn, StringComparison.OrdinalIgnoreCase))
+        {
+            writer.Close();
+
+            outputFile = string.Format(outputFileFormat, counter++);
+            writer = new StreamWriter(outputFile);
+        }
+
+        writer.WriteLine(line);
+    }
+}
+
+writer.Close();
--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # Code Analyses Tools
 
-## SolutionProjectChecker
+These are some handy tools and scripts I've used in the past to perform code analyses.
+
+>NOTE: The solutions mostly have hardcodes paths to folders and files, so you might need to change these.
+
+## File Splitter
+
+Can split a file in multiple smaller files. For instance it can split a large .sql file in smaller ones, splitting on the keyword `GO`.
+
+## Solution Project Checker
 
 Has features
 1. Checking which project files in a certain folder are not linked in a specific solution file.
