f69591699603b7234ec53770a9ccf03385d568d4
Ronald Bosma <ronaldb@infosupport.com>
2022-03-03 11:29:13 +0100

    Get target framework from global.json if not found in project file

--- SolutionProjectChecker/SolutionProjectChecker/TargetFrameworkCounter.cs
+++ SolutionProjectChecker/SolutionProjectChecker/TargetFrameworkCounter.cs
@@ -7,14 +7,14 @@ namespace SolutionProjectChecker
     /// </summary>
     internal class TargetFrameworkCounter
     {
+        private const string rootFolder = @"C:\repos\foo";
+        private const string outputFile = @"C:\repos\foo\target-frameworks.csv";
+        private const bool lookForGlobalJson = true;
+
         public static void Run()
         {
-            const string rootFolder = @"C:\repos\foo";
-            const string outputFile = @"C:\repos\foo\target-frameworks.csv";
-
             Regex targetFrameworkVersionPattern = new Regex(@"<TargetFrameworkVersion>([A-Za-z0-9\.-]+)<\/TargetFrameworkVersion>");
             Regex targetFrameworkPattern = new Regex(@"<TargetFramework>([A-Za-z0-9\.-]+)<\/TargetFramework>");
-
 
             var csprojFiles = Directory.GetFiles(rootFolder, "*.csproj", SearchOption.AllDirectories);
             var vbprojFiles = Directory.GetFiles(rootFolder, "*.vbproj", SearchOption.AllDirectories);
@@ -40,6 +40,12 @@ namespace SolutionProjectChecker
                         targetFrameworkVersion = match.Groups[1].Value;
                     }
 
+                    if (string.IsNullOrWhiteSpace(targetFrameworkVersion) && lookForGlobalJson)
+                    {
+                        var directory = Directory.GetParent(projFile)?.FullName;
+                        targetFrameworkVersion = GetTargetFrameworkVersionFromGlobalJson(directory);
+                    }
+
                     targetFrameworks.Add(projFile, targetFrameworkVersion);
                 }
             }
@@ -53,6 +59,34 @@ namespace SolutionProjectChecker
                     writer.WriteLine($"{item.Key.Replace(rootFolder, ".")}; {item.Value}");
                 }
             }
+        }
+
+        private static string GetTargetFrameworkVersionFromGlobalJson(string? directory)
+        {
+            if (directory == null || directory.Equals(rootFolder, StringComparison.OrdinalIgnoreCase))
+            {
+                return string.Empty;
+            }
+
+            var globalJsonPath = Path.Combine(directory, "global.json");
+
+            if (File.Exists(globalJsonPath))
+            {
+                using (var reader = new StreamReader(globalJsonPath))
+                {
+                    var content = reader.ReadToEnd();
+                    
+                    Regex versionPattern = new Regex(@"""version"": ""([A-Za-z0-9\.-]+)""");
+                    var match = versionPattern.Match(content);
+
+                    if (match.Success && match.Groups.Count > 1)
+                    {
+                        return match.Groups[1].Value;
+                    }
+                }
+            }
+
+            return GetTargetFrameworkVersionFromGlobalJson(Directory.GetParent(directory)?.FullName);
         }
     }
 }
